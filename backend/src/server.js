142eede7d08c78cf3dda338c969c7b502c66cebd
Arth9vansola <arthvansola@gmail.com>
2025-11-30 10:11:15 +0530

    second commit

---
+++ backend/src/server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { ENV } from './lib/env.js';
+
+const app = express();
+
+app.get('/health', (req, res) => {
+  res.status(200).json({ message: 'Server is running and api is good' });
+});
+
+app.listen(ENV.PORT, () => {
+  console.log("Server is listening on port :", ENV.PORT);
+} );
