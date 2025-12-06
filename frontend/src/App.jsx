667aa16f38d460cae502fca70e32cbcb12b6ac4d
Arth9vansola <arthvansola@gmail.com>
2025-12-06 12:56:41 +0530

    everything is done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
-import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton, useUser } from "@clerk/clerk-react";
+import { useUser } from "@clerk/clerk-react";
 import { Navigate, Route, Routes } from "react-router";
-import HomePage from "./pages/HomePage.jsx";
-import ProblemsPage from "./pages/ProblemsPage.jsx";
-import DashboardPage from "./pages/DashboardPage.jsx";
-import ProblemPage from "./pages/ProblemPage.jsx";
+import HomePage from "./pages/HomePage";
 import { Toaster } from "react-hot-toast";
+import DashboardPage from "./pages/DashboardPage";
+import ProblemPage from "./pages/ProblemPage";
+import ProblemsPage from "./pages/ProblemsPage";
+import SessionPage from "./pages/SessionPage";
 
 function App() {
 
@@ -22,6 +23,7 @@ function App() {
       
       <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />} />
       <Route path="/problem/:id" element={isSignedIn ? <ProblemPage /> : <Navigate to={"/"} />} />
+      <Route path="/session/:id" element={isSignedIn ? <SessionPage /> : <Navigate to={"/"} />} />
 
     </Routes>
     <Toaster toastOptions={{duration:3000}}/>
